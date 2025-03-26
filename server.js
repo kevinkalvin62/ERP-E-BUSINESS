fd7dd4503d60cc2cc653ebdcb1b97acc1e794bac
JeysonPill <caceresjeyson8@gmail.com>
2025-03-26 08:38:58 -0600

    Correccion estilos, Intento 1 Cargar Datos en tablas

--- server.js
+++ server.js
@@ -95,3 +95,13 @@ app.post('/login', (req, res) => {
     });
   });
 });
+
+app.get('/MOSTRADOR/buscar-objetos/', async (req, res) => {
+  try {
+      const [rows] = await db.query('SELECT * FROM PRODUCTOS');
+      res.json(rows);
+  } catch (error) {
+      console.error('Error al obtener registros:', error);
+      res.status(500).json({ error: 'Error en el servidor' });
+  }
+});
